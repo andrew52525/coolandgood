0cd2bd9147cb6cf1a9c18fc9b3f0fa27a3735e44
Andrew Chen <Andrew Chen>
2017-05-09 20:53:44 -0400

    hw12

---
+++ 12tree/StackCalc.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class StackCalc{
+    
+    public static double eval(String s){
+	String[] str = s.split(" ");
+	Stack st = new Stack();
+        for(int i = 0; i < str.length; i++){
+	    if(!(str[i].equals("+") || str[i].equals("*") || str[i].equals("/") || str[i].equals("-") || str[i].equals("%"))){
+		Double x = new Double(Double.parseDouble(str[i]));
+		st.push(x);
+	    }
+	    else{
+		Double b = (Double)st.pop();
+		Double a = (Double)st.pop();
+		st.push(operate(a, b, str[i]));
+	    }
+	}
+	return ((Double)st.pop()).doubleValue();
+    }
+
+    public static double operate(Double c, Double d, String op){
+	double a = c.doubleValue();
+	double b = d.doubleValue();
+	if(op.equals("*")){
+	    return a * b;
+	}
+	if(op.equals("/")){
+	    return a / b;
+	}
+	if(op.equals("%")){
+	    return a % b;
+	}
+	if(op.equals("+")){
+	    return a + b;
+	}
+	if(op.equals("-")){
+	    return a - b;
+	}
+	return 0.0;
+    }
+
+}
